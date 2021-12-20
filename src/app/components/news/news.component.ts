113a9a38211821986e7226da2e2399d9a73d7b29
Alisher Alseiit <Your Name>
2021-12-20 21:17:58 +0600

    news page

---
+++ src/app/components/news/news.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ProductService} from "../../services/product.service";
+import {NewsService} from "../../services/news.service";
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class ArticleComponent implements OnInit {
+  id: string | undefined;
+  article: any;
+  loading = true;
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private service: NewsService
+  ) { }
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.id = this.activeRoute.snapshot.paramMap.get('id');
+    this.getArticle();
+  }
+
+  getArticle(): void{
+    // @ts-ignore
+    this.service.get(this.id).subscribe(res => {
+      this.article = res;
+      this.loading = false
+      console.log(res);
+    });
+  }
+
+}
